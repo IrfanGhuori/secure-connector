4969e4c6b0b25b5d1ca7be86e18822d4a1861d07
Irfan Ghuori <42720620+IrfanGhuori@users.noreply.github.com>
2024-08-23 11:21:27 +0500

    add files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pdo/connecter',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'a57b50eb63b7c30fd6f510f3c4a2eb950e4cee09',
+        'type' => 'database',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pdo/connecter' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a57b50eb63b7c30fd6f510f3c4a2eb950e4cee09',
+            'type' => 'database',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
